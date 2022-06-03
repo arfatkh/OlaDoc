e74836e6edc1ee2560ac1ea63d52c855937e8df6
Muhammad Arfat <i210554@nu.edu.pk>
2022-06-04 00:03:42 +0500

    A little Testing with menu class and functions

--- OlaDoc/OlaDoc.cpp
+++ OlaDoc/OlaDoc.cpp
@@ -3,6 +3,19 @@
 void OlaDoc::startProgram()
 {
 	MenuMain.displayMainMenu();
+	MenuMain.displayDoctorMenu();
 
 
+}
+
+
+int main()
+{
+	OlaDoc OlaDoc1;
+
+
+	OlaDoc1.startProgram();
+
+
+	return 0;
 }
